d7774fb4e46747cc4a78248e21ce530c2012594b
herreralorenz <herreralorenz@yahoo.com>
2021-11-10 09:52:18 +0800

    Add files via upload

---
+++ js/jqvalidator.js
@@ -0,0 +1,30 @@
+$("#regForm").validate({
+    rules: {
+        category: "required",
+        pwd: "required",
+        indigenous: "required",
+        lastname: "required",
+        firstname: "required",
+        middlename: "required",
+        suffix: "required",
+        dob: "required",
+        age: "required",
+        sex: "required",
+        civilstatus: "required",
+        completeaddress: "required",
+        region: "required",
+        province: "required",
+        city: "required",
+        barangay: "required",
+        mobilenumber: "required",
+        nameofguardian: "required",
+        guardiancontactnumber: "required",
+        emailaddress: "required"
+    },
+    messages: {
+       
+
+
+    
+    }
+});
